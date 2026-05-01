64732b3b00ff4c2c4f5c51da1d42c4b02b95f12b
patelkrenil11 <patelkrenil150@gmail.com>
2026-05-01 14:11:15 +0530

    style(fonts): revert display font back to Bricolage Grotesque (400-800)

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,4 +1,4 @@
-import { DM_Sans, JetBrains_Mono, Plus_Jakarta_Sans } from "next/font/google";
+import { DM_Sans, JetBrains_Mono, Bricolage_Grotesque } from "next/font/google";
 
 export const inter = DM_Sans({
   subsets: ["latin"],
@@ -14,7 +14,7 @@ export const jetbrainsMono = JetBrains_Mono({
   weight: ["400", "500", "700"],
 });
 
-export const syne = Plus_Jakarta_Sans({
+export const syne = Bricolage_Grotesque({
   subsets: ["latin"],
   variable: "--font-syne",
   display: "swap",
